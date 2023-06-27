79018ebbfc1b11689ab4d6bb322778e4aad0caaa
Sachin Sharma <90981866+semantic-void@users.noreply.github.com>
2023-06-27 16:58:41 +0530

    Delete GeekCentral/GeekCentral directory

--- GeekCentral/GeekCentral/src/main/java/com/sachin/GeekCentral/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sachin.GeekCentral.repository;
-
-import com.sachin.GeekCentral.entity.Category;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-
-}
